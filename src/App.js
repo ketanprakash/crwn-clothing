a8cc90b82e256bcc191648d4b0fba685ac1cc63b
ketanprakash <ketanprakash001@gmail.com>
2021-12-12 14:25:04 +0530

    updating our app with a shop page, a collection item component, a collection preview component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,17 @@
+import { Routes, Route } from 'react-router';
+
 import './App.css';
 
 import {HomePage} from './pages/homepage/homepage.component'
-function App() {
-  return (
-    <div>
-     <HomePage/> 
-    </div>
-  );
-}
+import {ShopPage} from './pages/shop/shop.component'
+
+const App = () => (
+  <div>
+    <Routes>
+      <Route path = '/' element = {<HomePage/>}/>
+      <Route path = '/shop' element = {<ShopPage/>}></Route>
+    </Routes>
+  </div>
+)
 
 export default App;
---
+++ src/pages/shop/shop.component.jsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import SHOP_DATA from './shop.data'
+import { CollectionPreview } from '../../components/collection-preview/collection-preview.component';
+export class ShopPage extends React.Component{
+  constructor(props){
+    super(props);
+
+    this.state = {
+      collections: SHOP_DATA
+    }
+  }
+
+  render(){
+    return (
+      <div className = "shop-page">
+        {
+          this.state.collections.map(({id, ...otherCollectionsProps}) => (
+            <CollectionPreview key={id} {...otherCollectionsProps}/>
+          ))
+        }
+      </div>
+    )
+  }
+}
